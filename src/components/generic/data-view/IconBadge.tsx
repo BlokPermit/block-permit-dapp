d6c2dbe2e1ee3f62997b6cb11355288142bf9e61
Sasa Vincic <sasa.vincic16@gmail.com>
2023-05-31 10:08:00 +0200

    fix(IconBadge): Fixed IconBadge functionality.

--- src/components/generic/data-view/IconBadge.tsx
+++ src/components/generic/data-view/IconBadge.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { FaArrowUp, FaCheck } from "react-icons/fa";
 
 interface IconBadgeProps {
   icon: any;
@@ -8,34 +7,32 @@ interface IconBadgeProps {
 }
 
 const IconBadge = (props: IconBadgeProps) => {
-  let [textColor, backgroundColor] = ["bg-green-100", "text-green-700"];
+  let [bgColor, textColor] = ["bg-green-100", "text-green-700"];
 
   useEffect(() => {
     switch (props.badgeType) {
       case "success":
-        [textColor, backgroundColor] = ["bg-green-100", "text-green-700"];
+        [bgColor, textColor] = ["bg-green-100", "text-green-700"];
         break;
       case "warning":
-        [textColor, backgroundColor] = ["bg-yellow-100", "text-yellow-700"];
+        [bgColor, textColor] = ["bg-yellow-100", "text-yellow-700"];
         break;
       case "danger":
-        [textColor, backgroundColor] = ["bg-red-100", "text-red-700"];
+        [bgColor, textColor] = ["bg-red-100", "text-red-700"];
         break;
       case "info":
-        [textColor, backgroundColor] = ["bg-blue-100", "text-blue-700"];
+        [bgColor, textColor] = ["bg-blue-100", "text-blue-700"];
         break;
       default:
-        [textColor, backgroundColor] = ["bg-green-100", "text-green-700"];
+        [bgColor, textColor] = ["bg-green-100", "text-green-700"];
         break;
     }
   }, [props.badgeType]);
 
   return (
-    <span className={textColor + backgroundColor}>
-      <span className="inline-flex items-center justify-center whitespace-nowrap rounded-full px-2.5 py-0.5 text-sm ">
-        <span className="ms-1 me-1.5">{props.icon}</span>
-        <span className="me-1">{props.text}</span>
-      </span>
+    <span className={bgColor.concat(" " + textColor).concat(" inline-flex items-center justify-center whitespace-nowrap rounded-full px-2.5 py-0.5 text-sm")}>
+      <span className="ms-1 me-1.5">{props.icon}</span>
+      <span className="me-1">{props.text}</span>
     </span>
   );
 };
