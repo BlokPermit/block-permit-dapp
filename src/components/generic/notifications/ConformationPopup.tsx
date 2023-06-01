702500e2d1d83f9522f1ef766ba55b00d261d543
Sasa Vincic <sasa.vincic16@gmail.com>
2023-06-01 13:25:42 +0200

    fix(ConformationPopup): Fixed Conformation Popup colors

--- src/components/generic/notifications/ConformationPopup.tsx
+++ src/components/generic/notifications/ConformationPopup.tsx
@@ -4,24 +4,9 @@ import { useEffect, useState } from "react";
 const ConformationPopup = () => {
   const props = useConformationPopup();
   const [show, setShow] = useState<boolean>(false);
-  const [color, setColor] = useState<string>("");
+
   useEffect(() => {
     setShow(props.show);
-    switch (props.popupType) {
-      case "success":
-        setColor("green-300");
-        break;
-      case "error":
-        setColor("red-400");
-        break;
-      case "warning":
-        setColor("main-200");
-        break;
-      case "info":
-        setColor("gray-200");
-        break;
-      default:
-    }
   }, [props]);
 
   return (
@@ -30,12 +15,27 @@ const ConformationPopup = () => {
         <div className="fixed z-50 top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
           <div className="max-w-md w-full rounded-2xl bg-white p-4 shadow-lg sm:p-6 lg:p-8 relative" role="alert">
             <div className="flex items-center gap-4">
-              <span className={`shrink-0 rounded-full p-2 text-${color}`}>{props.icon}</span>
+              <span
+                className={`shrink-0 rounded-full p-2
+              ${props.popupType == "success" && "text-green-600"} 
+              ${props.popupType == "error" && "text-red-600"} 
+              ${props.popupType == "warning" && "text-main-200"} 
+              ${props.popupType == "info" && "text-gray-200"}`}
+              >
+                {props.icon}
+              </span>
               <p className="font-medium sm:text-lg">{props.title}</p>
             </div>
             <p className="mt-4 text-gray-500">{props.message}</p>
             <div className="mt-6 sm:flex sm:gap-4">
-              <button className={`inline-block w-full rounded-lg px-5 py-3 text-center text-sm font-semibold text-white sm:w-auto bg-${color}`} onClick={props.onClickPrimary}>
+              <button
+                className={`inline-block w-full rounded-lg px-5 py-3 text-center text-sm font-semibold text-white sm:w-auto 
+                ${props.popupType == "success" && "bg-green-600"} 
+                ${props.popupType == "error" && "bg-red-600"} 
+                ${props.popupType == "warning" && "bg-main-200"} 
+                ${props.popupType == "info" && "bg-gray-200"}`}
+                onClick={props.onClickPrimary}
+              >
                 {props.buttonPrimaryText}
               </button>
 
