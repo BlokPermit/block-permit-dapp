8a7f639cb4605d7fb78e8d216e77ff3ca72ce9fd
Sasa Vincic <sasa.vincic16@gmail.com>
2023-06-02 14:59:02 +0200

    fix(IconButton): Removed unnecessary styling

--- src/components/generic/buttons/IconButton.tsx
+++ src/components/generic/buttons/IconButton.tsx
@@ -8,15 +8,10 @@ interface AltButtonProps {
 }
 const IconButton = (props: AltButtonProps) => {
   return (
-    <span className="flex justify-end">
-      <button
-        className={`${props.className} inline-flex items-center gap-2 rounded-md bg-white px-4 py-2 text-sm text-main-200 shadow-sm focus:relative hover:text-white hover:bg-main-200`}
-        onClick={props.onClick}
-      >
-        {props.icon}
-        {props.text}
-      </button>
-    </span>
+    <button className={`${props.className} inline-flex items-center gap-2 rounded-md px-4 py-2 text-sm shadow-sm focus:relative`} onClick={props.onClick}>
+      {props.icon}
+      {props.text}
+    </button>
   );
 };
 
