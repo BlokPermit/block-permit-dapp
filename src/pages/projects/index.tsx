216d453020282c5365373c7fc22d9edea1dbc817
Sasa Vincic <sasa.vincic16@gmail.com>
2023-06-04 13:57:08 +0200

    feature(ProgressBar): Refactored ProgressBar to use ProjectPhase enum. Added filtering to Projects table.

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -8,14 +8,22 @@ import { getAllProjects } from "@/lib/ProjectService";
 import { InferGetServerSidePropsType } from "next/types";
 import { Project } from "@prisma/client";
 import { useRouter } from "next/router";
+import ProgressBar from "@/components/specific/ProgressBar";
+import { ProjectModel, ProjectPhase } from "@/models/ProjectModel";
 
 export const getServerSideProps: any = async () => {
   try {
-    const projects: Project[] = await getAllProjects();
-    console.log(projects);
+    const baseProjects: Project[] = await getAllProjects();
+    const projects: ProjectModel[] = baseProjects.map((project: Project) => {
+      return {
+        baseProject: project,
+        projectPhase: ProjectPhase.PHASE_1,
+        assessmentProviders: [],
+      };
+    });
     return {
       props: {
-        projects,
+        projects: projects,
       },
     };
   } catch (e) {
@@ -25,6 +33,7 @@ export const getServerSideProps: any = async () => {
 
 const Projects = ({ projects }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
+  const [selectedPhase, setSelectedPhase] = React.useState<number>(1);
 
   return (
     <ProtectedRoute>
@@ -37,8 +46,11 @@ const Projects = ({ projects }: InferGetServerSidePropsType<typeof getServerSide
           <AnimatedIconButton text={"Add Project"} icon={<AiOutlineAppstoreAdd size={20} />} isLink={true} href={"/projects/addProject"}></AnimatedIconButton>
         </div>
         <div className="mt-6">
+          <div className="my-3">
+            <ProgressBar actualPhase={ProjectPhase.PHASE_3} selectedPhase={selectedPhase} handlePhaseChange={(phase: ProjectPhase) => setSelectedPhase(phase)} />
+          </div>
           <div className="overflow-x-auto rounded-lg border border-gray-200">
-            <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+            <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm text-left">
               <thead>
                 <tr>
                   <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Name</th>
@@ -49,15 +61,18 @@ const Projects = ({ projects }: InferGetServerSidePropsType<typeof getServerSide
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200">
-                {projects.map((project: Project) => (
-                  <tr className="hover:cursor-pointer hover:bg-gray-100" key={project.id} onClick={() => router.push(`/projects/${project.id}`)}>
-                    <td className="whitespace-nowrap px-4 py-2 text-gray-900">Proj-{project.id}</td>
-                    <td className="whitespace-nowrap px-4 py-2 text-gray-900">{project.constructionTitle}</td>
-                    <td className="whitespace-nowrap px-4 py-2 text-gray-900">{project.constructionType}</td>
-                    <td className="whitespace-nowrap px-4 py-2 text-gray-900">{project.constructionImpactsEnvironment ? "Yes" : "No"}</td>
-                    <td className="whitespace-nowrap px-4 py-2 text-gray-900">Phase 3: Final Phase</td>
-                  </tr>
-                ))}
+                {projects.map(
+                  (project: ProjectModel) =>
+                    project.projectPhase == selectedPhase && (
+                      <tr className="hover:cursor-pointer hover:bg-gray-100" key={project.baseProject.id} onClick={() => router.push(`/projects/${project.baseProject.id}`)}>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-900">Proj-{project.baseProject.id}</td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-900">{project.baseProject.constructionTitle}</td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-900">{project.baseProject.constructionType}</td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-900">{project.baseProject.constructionImpactsEnvironment ? "Yes" : "No"}</td>
+                        <td className="whitespace-nowrap px-4 py-2 text-gray-900">{project.projectPhase}</td>
+                      </tr>
+                    )
+                )}
               </tbody>
             </table>
           </div>
