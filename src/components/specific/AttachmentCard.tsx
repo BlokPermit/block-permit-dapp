2e2817c49af96fe1f3dc03c1a8a3f5113c368ea6
Sasa Vincic <sasa.vincic16@gmail.com>
2023-06-07 17:19:35 +0200

    feature(AttachmentPopup): Download Attachment

--- src/components/specific/AttachmentCard.tsx
+++ src/components/specific/AttachmentCard.tsx
@@ -1,3 +1,5 @@
+import useAlert from "@/hooks/AlertHook";
+import { downloadDocument } from "@/lib/DocumentService";
 import { useState } from "react";
 import { FaArrowDown, FaPaperclip, FaRegTrashAlt, FaTrash, FaTrashAlt, FaTrashRestore } from "react-icons/fa";
 
@@ -6,13 +8,29 @@ interface AttachmentCardProps {
 }
 
 const AttachmentCard = (props: AttachmentCardProps) => {
+  const { setAlert } = useAlert();
   const [isDownloadHovered, setIsDownloadHovered] = useState(false);
-  const [isRemoveHovered, setIsRemoveHovered] = useState(false);
-  const downloadAttachment = () => {};
+  const downloadAttachment = async () => {
+    try {
+      if (props.attachment.attachmentPath != null) {
+        const file: Blob | boolean = await downloadDocument(props.attachment.attachmentPath);
+        if (file == false) throw Error("Failed to download document. Please try again.");
+
+        const blobURL = window.URL.createObjectURL(file as Blob);
+
+        const downloadLink = document.createElement("a");
+        downloadLink.href = blobURL;
+        downloadLink.setAttribute("download", props.attachment.attachmentTitle);
+        downloadLink.click();
+      }
+    } catch (error: Error | any) {
+      setAlert({ title: "", message: error.message, type: "error" });
+    }
+  };
   const removeAttachment = () => {};
 
   return (
-    <div className={"flex justify-between items-center rounded-lg bg-white border-2 mb-4 ".concat(isDownloadHovered ? "border-main-200" : "border-gray-200")}>
+    <div className={`flex justify-between items-center rounded-lg bg-white border-2 mb-4 ${isDownloadHovered ? "border-main-200" : "border-gray-200"}`}>
       <div
         onClick={downloadAttachment}
         className="inline-flex items-center gap-2 p-3 hover:border-main-200 hover:cursor-pointer hover:text-main-200"
