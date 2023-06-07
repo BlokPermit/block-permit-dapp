4a3ca284e7f10078b95221bc9570231aab333b0c
PolnerFilip <filip.polner@student.um.si>
2023-06-07 13:44:07 +0200

    feature(DocumentService): fetching paths from Supabase

--- src/lib/DocumentService.ts
+++ src/lib/DocumentService.ts
@@ -2,28 +2,56 @@ import {supabase} from "@/utils/SupabaseClient";
 
 export const saveDocument = async (file: File | null, path: string) => {
     if (file != null) {
-        const {data, error} = await supabase.storage
-            .from("blokcejn-bucket")
-            .upload(`public/${path}/${file.name}`, file, {
-                cacheControl: "3600",
-                upsert: false,
-            });
+        try {
+            const {data, error} = await supabase.storage
+                .from("blokcejn-bucket")
+                .upload(`public/${path}/${file.name}`, file, {
+                    cacheControl: "3600",
+                    upsert: false,
+                });
 
-        if (error) {
-            return false;
+            if (error) {
+                return false;
+            }
+            return data.path;
+        } catch (e: any) {
+            console.log(e);
+            throw e;
         }
-        return data.path;
     } else return false;
 };
 
 export const downloadDocument = async (path: string): Promise<Blob | boolean> => {
-    const {data, error} = await supabase.storage
-        .from("blokcejn-bucket")
-        .download(path);
+    try {
+        const {data, error} = await supabase.storage
+            .from("blokcejn-bucket")
+            .download(path);
 
-    if (error) {
-        return false;
+        if (error) {
+            return false;
+        }
+
+        return data;
+    } catch (e: any) {
+        console.log(e);
+        throw e;
     }
+}
 
-    return data;
+export const getAllFilesInDirectory = async (path: string): Promise<string[] | boolean> => {
+    try {
+        const {data, error} = await supabase.storage
+            .from("blokcejn-bucket")
+            .list(path);
+
+        if (error) {
+            return false;
+        }
+
+        console.log(data);
+        return data.map(file => `${path}/${file.name}`);
+    } catch (e: any) {
+        console.log(e);
+        throw e;
+    }
 }
