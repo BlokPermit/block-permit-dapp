ecdcc391d095db9c7dec1f16fbc9eacaa4dc2a1e
Sasa Vincic <sasa.vincic16@gmail.com>
2023-06-09 13:10:23 +0200

    feature(AssessmentProviderInfo): Show Requested Assessment Due Date

--- src/components/specific/AssessmentProviderInfoPopup.tsx
+++ src/components/specific/AssessmentProviderInfoPopup.tsx
@@ -78,7 +78,8 @@ const AssessmentProviderInfoPopup = (props: AssessmentProviderInfoPopupProps) =>
               </h1>
               <div>
                 {props.documentContract.requestedAssessmentDueDate && (
-                  <div className="inline-flex gap-3">
+                  <div className="inline-flex items-center gap-3">
+                    <p className="me-1 text-lg text-gray-500 font-semibold">{props.documentContract.requestedAssessmentDueDate}</p>
                     <IconButton
                       className="text-white bg-green-700 hover:text-green-700 hover:bg-white"
                       text={"Potrdi podaljšanje roka"}
